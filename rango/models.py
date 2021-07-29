f173bc2eb59ddf08d30b172200731cddb9ff52a5
CassieX35 <2583731X@student.gla.ac.uk>
2021-07-29 12:19:48 +0100

    For Chapter5.1-5.8

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category (models.Model):
+    name = models.CharField(max_length=128, unique=True) # unique=True is equal to primary key
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) # page's catecory 
+    title = models.CharField(max_length=128) # page's title
+    url = models.URLField() # the url of page
+    views = models.IntegerField(default=0) # visiting number of this page
+
+    def __str__(self):
+        return self.title
